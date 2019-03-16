70f262aa261881be8f79ed6fcaf61042879349d1
Andrea <andrzej.sedzik.dev@gmail.com>
2019-03-17 00:09:06 +0100

    fix

--- resources/views/Email/passwordReset.blade.php
+++ resources/views/Email/passwordReset.blade.php
@@ -1,15 +1,13 @@
-@component('mail::message')
-    # Change password Request
-
-    Click on the button below to change password
-@endcomponent
-
 
 
     @component('mail::button', ['url' => 'angular-deploy2020.herokuapp.com/response-password-reset?token='.$token])
         Reset Password
-    Thanks,<br>
-    {{ config('app.name') }}
     @endcomponent
 
 
+
+
+
+
+{{--
+  @component('mail::button', ['url' => 'angular-deploy2020.herokuapp.com/response-password-reset?token='.$token])--}}
